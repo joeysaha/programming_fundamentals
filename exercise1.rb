5120ebea34986226f844723c94555cd07fa8973c
Joey Saha <joeysaha@hotmail.com>
2018-07-16 17:04:30 -0400

    initial commit

---
+++ exercise1.rb
@@ -0,0 +1,44 @@
+# puts 2
+# puts 3
+# puts 2+3
+#
+# "Betty's pie shop"
+# 'Betty\'s pie shop'
+#
+# puts "Ada Lovelace lived for #{1852 - 1815} years."
+#
+# puts "Betty's pie sh\aop"
+# puts "Betty's pie sh\bop"
+# puts "Betty's pie sh\rop"
+# puts "Betty's pie sh\nop"
+# puts "Betty's pie sh\sop"
+# puts "Betty's pie sh\top"
+#
+# puts "Hello\t\tworld"
+# puts "Hello\b\b\b\b\bGoodbye world"
+# puts "Hello\rStart over world"
+# puts "1. Hello\n2. World"
+#
+# puts "Hello" + "world"
+# puts "Hello" * "world"
+# puts "Hello" / "world"
+# puts "Hello" - "world"
+# puts "Hello" % "world"
+# puts "hello" > "world"
+# puts "hello" <= "world"
+# puts "hello" == "world"
+
+# puts true && true
+# puts true && false
+# puts true && !false
+# puts true || false
+# puts true || true
+# puts false || true
+# puts false || false
+# puts !false || false
+
+puts (1<3) && (3<5)
+puts (1>1)&&(2>2)
+puts (1==2)||(2<3)
+puts (1!=2)||(2<3)
+puts (1==2)||(2==3)
---
+++ exercise2.rb
@@ -0,0 +1,61 @@
+# price
+# price = 55
+#tip
+# puts price*1.15
+#
+# puts "What is your favorite number?" + price.to_s
+# puts "The result of " + 45628.to_s + " x " + 7839.to_s + " is #{45628*7839}"
+#
+# puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+# my_variable = 'my_variable now contains this string'
+
+# name = "Sandra"
+# greeting = "Hello #{name}! It's good to see you again."
+# mission = "Your mission, should you choose to accept it..."
+#
+# puts "#{greeting} #{mission}"
+
+# my_number = 12
+# puts my_number > 10
+# puts my_number < 10
+#
+# your_number = 1
+#
+# puts my_number == your_number
+#
+# puts my_number != your_number
+
+# amount = 20
+# new_amount = amount
+#
+# amount = "twenty"
+#
+# puts amount
+# puts new_amount
+#
+# animals = "cats"
+# number = 20
+# location = "the yard"
+#
+# puts "There are #{number} #{animals} in #{location}"
+#
+# who = "Mrs. Peacock"
+# where = "the library"
+# what = "rope"
+#
+# accusation = "It was #{who} in #{where} with the #{what}."
+# puts accusation
+#
+# counter = 25
+# # counter = counter + 1
+# counter += 1
+# puts counter
+#
+# amount = 1
+# amount += 10
+# puts amount
+#
+# amount = 30
+# amount -= 5
+# puts amount
